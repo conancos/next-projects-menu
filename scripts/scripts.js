eab5f4c57f0c2b1a4585e13e060eec68bb9718e2
15524757+conancos@users.noreply.github.com <15524757+conancos@users.noreply.github.com>
2024-02-08 12:12:54 +0100

    Vulnerability discovered in images and fix it

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,9 +1,14 @@
 const images = document.querySelectorAll("img");
 /* console.log(images) */
-
-
 images.forEach(image => {
     image.addEventListener('contextmenu', function (event) {
         event.preventDefault();
     });
 });
+
+// Desactivo contextmenu en .cards-container logic y DOM
+const cards_con = document.querySelectorAll('.cards-container').forEach(element => {
+    element.addEventListener('contextmenu', function (e) {
+        e.preventDefault();
+    })
+})
